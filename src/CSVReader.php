280154b596b8edde914a237fcb70f3b7d0fe9a31
Dan Smith <daniel.smith@magic42.co.uk>
2020-01-08 11:12:06 +0000

    Change tab spacing from 2 to 4 characters

--- src/CSVReader.php
+++ src/CSVReader.php
@@ -2,103 +2,108 @@
 
 class CSVReader
 {
-  private $filePath;
-  private $headers;
-  private $data;
+    private $filePath;
+    private $headers;
+    private $data;
 
-  /**
-   * Initialiase the CsvReader Object
-   *
-   * @param string $filePath
-   */
-  public function __construct(string $filePath)
-  {
-    $this->filePath = $filePath;
-    $this->headers = null;
-    $this->data = null;
+    /**
+     * Initialiase the CsvReader Object
+     *
+     * @param string $filePath
+    */
+    public function __construct(string $filePath)
+    {
+        $this->filePath = $filePath;
+        $this->headers = null;
+        $this->data = null;
 
-    if(!$this->isValid()) {
-      throw new Exception('FilePath provided is not valid');
-    }
-  }
-
-  /**
-   * Get the headers of the csv file
-   *
-   * @return array
-   */
-  public function getHeaders() : array
-  {
-    if ($this->headers === null) {
-      $this->readToArray();
-    }
-    return $this->headers;
-  }
-
-  /**
-   * Get the csv as an associative array
-   *
-   * @return array
-   */
-  public function asArray() : array
-  {
-    if ($this->data === null) {
-      $this->readToArray();
-    }
-    return $this->data;
-  }
-
-  /**
-   * Check if the file path provided is a valid csv file
-   *
-   * @return bool
-   */
-  private function isValid() : bool
-  {
-    if (substr($this->filePath, -4) !== '.csv') {
-      return false;
+        if(!$this->isValid()) {
+            throw new Exception('FilePath provided is not valid');
+        }
     }
 
-    if (!file_exists($this->filePath)) {
-      return false;
+    /**
+     * Get the headers of the csv file
+     *
+     * @return array
+    */
+    public function getHeaders() : array
+    {
+        if ($this->headers === null) {
+            $this->readToArray();
+        }
+
+        return $this->headers;
     }
 
-    return true;
-  }
+    /**
+     * Get the csv as an associative array
+     *
+     * @return array
+    */
+    public function asArray() : array
+    {
+        if ($this->data === null) {
+            $this->readToArray();
+        }
 
-  /**
-   * Reads the csv file into an associative array.
-   * Stored in the $this->data variable.
-   *
-   * @return bool if the function is successful
-   */
-  private function readToArray() : bool
-  {
-    if (!$handle = fopen($this->filePath, 'r')) {
-      return false;
+        return $this->data;
     }
 
-    $result = null;
-    $headers = null;
-    while ($row = fgetcsv($handle)) {
-      if ($headers === null) {
-        $headers = $row;
-        continue;
-      }
-      $newRow = [];
-      for ($col = 0; $col < count($row); $col++) {
-        $newRow[$headers[$col]] = $row[$col];
-      }
-      $result[] = $newRow;
+    /**
+     * Check if the file path provided is a valid csv file
+     *
+     * @return bool
+    */
+    private function isValid() : bool
+    {
+        if (substr($this->filePath, -4) !== '.csv') {
+            return false;
+        }
+
+        if (!file_exists($this->filePath)) {
+            return false;
+        }
+
+        return true;
     }
 
-    fclose($handle);
+    /**
+     * Reads the csv file into an associative array.
+     * Stored in the $this->data variable.
+     *
+     * @return bool if the function is successful
+    */
+    private function readToArray() : bool
+    {
+        if (!$handle = fopen($this->filePath, 'r')) {
+            return false;
+        }
 
-    $this->headers = $headers;
-    $this->data = $result;
+        $result = null;
+        $headers = null;
 
-    return true;
-  }
+        while ($row = fgetcsv($handle)) {
+            if ($headers === null) {
+                $headers = $row;
+                continue;
+            }
+
+            $newRow = [];
+            for ($col = 0; $col < count($row); $col++) {
+                $newRow[$headers[$col]] = $row[$col];
+            }
+
+            $result[] = $newRow;
+        }
+
+        fclose($handle);
+
+        $this->headers = $headers;
+        $this->data = $result;
+
+        return true;
+    }
 }
 
 ?>
--- tests/CSVReaderTest.php
+++ tests/CSVReaderTest.php
@@ -6,82 +6,83 @@ use PHPUnit\Framework\TestCase;
 
 class CSVReaderTest extends TestCase
 {
-  private $testCsv;
+    /** CSVReader $testCsv */
+    private $testCsv;
 
-  public function setUp()
-  {
-    $this->testCsv = new CSVReader('data/test.csv');
-  }
+    public function setUp()
+    {
+        $this->testCsv = new CSVReader('data/test.csv');
+    }
 
-  public function tearDown()
-  {
-    unset($this->testCsv);
-  }
+    public function tearDown()
+    {
+        unset($this->testCsv);
+    }
 
-  /**
-   *  @expectedException Exception
-   */
-  public function testNonExistingFileException()
-  {
-    $csv = new CSVReader('data/non_existing_file.csv');
-  }
+    /**
+     *  @expectedException Exception
+    */
+    public function testNonExistingFileException()
+    {
+        $csv = new CSVReader('data/non_existing_file.csv');
+    }
 
-  /**
-   *  @expectedException Exception
-   */
-  public function testNonCsvFileException()
-  {
-    $csv = new CSVReader('data/non_csv_file.docx');
-  }
+    /**
+     *  @expectedException Exception
+    */
+    public function testNonCsvFileException()
+    {
+        $csv = new CSVReader('data/non_csv_file.docx');
+    }
 
-  /**
-   * Test the correct headers are stored from reading the csv file
-   */
-  public function testHeaders()
-  {
-    $this->assertEquals(
-      ['id','firstName','lastName','email'],
-      $this->testCsv->getHeaders()
-    );
-  }
+    /**
+     * Test the correct headers are stored from reading the csv file
+    */
+    public function testHeaders()
+    {
+        $this->assertEquals(
+            ['id','firstName','lastName','email'],
+            $this->testCsv->getHeaders()
+        );
+    }
 
-  /**
-   * Test a given record exists when read from the csv file
-   *
-   * @dataProvider recordProvider
-   */
-  public function testRecordExists(array $record)
-  {
-    $this->assertContains($record, $this->testCsv->asArray());
-  }
+    /**
+     * Test a given record exists when read from the csv file
+     *
+     * @dataProvider recordProvider
+    */
+    public function testRecordExists(array $record)
+    {
+        $this->assertContains($record, $this->testCsv->asArray());
+    }
 
-  /**
-   * Provides test records to see if they exist when they are read in
-   * @see CSVReaderTest::testRecordExists
-   *
-   * @return array the records to test
-   */
-  public function recordProvider() : array
-  {
-    return [
-      [[
-        'id' => '1',
-        'firstName' => 'daniel',
-        'lastName' => 'smith',
-        'email' => 'daniel.smith@magic42.co.uk',
-      ]],
-      [[
-        'id' => '2',
-        'firstName' => 'bob',
-        'lastName' => 'marley',
-        'email' => 'bob.marley@email.com',
-      ]],
-      [[
-        'id' => '3',
-        'firstName' => 'jess',
-        'lastName' => 'jones',
-        'email' => 'jessica.jones99@email.com',
-      ]],
-    ];
-  }
+    /**
+     * Provides test records to see if they exist when they are read in
+     * @see CSVReaderTest::testRecordExists
+     *
+     * @return array the records to test
+    */
+    public function recordProvider() : array
+    {
+        return [
+            [[
+                'id' => '1',
+                'firstName' => 'daniel',
+                'lastName' => 'smith',
+                'email' => 'daniel.smith@magic42.co.uk',
+            ]],
+            [[
+                'id' => '2',
+                'firstName' => 'bob',
+                'lastName' => 'marley',
+                'email' => 'bob.marley@email.com',
+            ]],
+            [[
+                'id' => '3',
+                'firstName' => 'jess',
+                'lastName' => 'jones',
+                'email' => 'jessica.jones99@email.com',
+            ]],
+        ];
+    }
 }
