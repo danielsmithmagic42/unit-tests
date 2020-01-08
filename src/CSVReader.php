fb2ca8d8bd42fe851a2b042ef3a901f85c1fc704
Dan Smith <daniel.smith@magic42.co.uk>
2020-01-08 10:09:30 +0000

    Create CSVReader class which will be tested

---
+++ src/CSVReader.php
@@ -0,0 +1,104 @@
+<?php
+
+class CSVReader
+{
+  private $filePath;
+  private $headers;
+  private $data;
+
+  /**
+   * Initialiase the CsvReader Object
+   *
+   * @param string $filePath
+   */
+  public function __construct(string $filePath)
+  {
+    $this->filePath = $filePath;
+    $this->headers = null;
+    $this->data = null;
+
+    if(!$this->isValid()) {
+      throw new Exception('FilePath provided is not valid');
+    }
+  }
+
+  /**
+   * Get the headers of the csv file
+   *
+   * @return array
+   */
+  public function getHeaders() : array
+  {
+    if ($this->headers === null) {
+      $this->readToArray();
+    }
+    return $this->headers;
+  }
+
+  /**
+   * Get the csv as an associative array
+   *
+   * @return array
+   */
+  public function asArray() : array
+  {
+    if ($this->data === null) {
+      $this->readToArray();
+    }
+    return $this->data;
+  }
+
+  /**
+   * Check if the file path provided is a valid csv file
+   *
+   * @return bool
+   */
+  private function isValid() : bool
+  {
+    if (substr($this->filePath, -4) !== '.csv') {
+      return false;
+    }
+
+    if (!file_exists($this->filePath)) {
+      return false;
+    }
+
+    return true;
+  }
+
+  /**
+   * Reads the csv file into an associative array.
+   * Stored in the $this->data variable.
+   *
+   * @return bool if the function is successful
+   */
+  private function readToArray() : bool
+  {
+    if (!$handle = fopen($this->filePath, 'r')) {
+      return false;
+    }
+
+    $result = null;
+    $headers = null;
+    while ($row = fgetcsv($handle)) {
+      if ($headers === null) {
+        $headers = $row;
+        continue;
+      }
+      $newRow = [];
+      for ($col = 0; $col < count($row); $col++) {
+        $newRow[$headers[$col]] = $row[$col];
+      }
+      $result[] = $newRow;
+    }
+
+    fclose($handle);
+
+    $this->headers = $headers;
+    $this->data = $result;
+
+    return true;
+  }
+}
+
+?>
